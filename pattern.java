efe0efe09b1e250c38166c2936b70af809f8acea
shifa7699 <111556272+shifa7699@users.noreply.github.com>
2022-09-27 11:53:45 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -4,14 +4,14 @@ public static void printTriangle(int n)//Method to print pattern
 {
 int i,j;
 //outer loop to handle the no of rows
-for(i=0;i<n;i++)
+for(i=0;i<=n;i++)
 {
 //Inner loop to handle number of space
-for(j=n-i;j>1;j--)
+for(j=n-i;j>=1;j--)
 {
 System.out.print(" ");
 }
-for(j=0;j<i;j++)
+for(j=0;j<=i;j++)
 {
 System.out.print(" * ");
 }
