7676d6c2540dec1183b94cb9cdadf6d31aace2e9
Shifa Iqbal <111556272+shifa7699@users.noreply.github.com>
2022-11-01 12:38:25 +0530

    Update DuplicateArray.java

--- DuplicateArray.java
+++ DuplicateArray.java
@@ -1,3 +1,6 @@
+/*Q.2   Write a Java program to find the duplicate values 
+      of an array of integer values.*/
+
 package pack.com;
 
 import java.util.Scanner;
@@ -9,21 +12,23 @@ public class DuplicateArray //main class
 		int a[]=new int [10];
 		int n,i,j;
 		for (i=0; i<a.length;i++) 
-        {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the numbers:- "); //Printing 10 numbers
-		a[i]=sc.nextInt();
-        }
+        	{
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter the numbers:- "); //Printing 10 numbers
+			a[i]=sc.nextInt();
+        	}
 		for (i = 0; i <a.length; i++) 
-        {
+        	{
 			for (j = i+1; j<a.length; j++) 
-            {
-                if (a[i] == a[j]) //Comparing the values of the two loops
-                {
-                    System.out.println("Duplicate Element : "+a[j]); //Printing the duplicate number if present
-                }
-            }
-		}
-		
+            	{
+                	if (a[i] == a[j]) //Comparing the values of the two loops
+                	{
+                   		 System.out.println("Duplicate Element : "+a[j]); //Printing the duplicate number if present
+                	}
+            	}
+		if(f==0)
+        	{
+        		System.out.println("NOT FOUND");
+        	}
 	}
 }
